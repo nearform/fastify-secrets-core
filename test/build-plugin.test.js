c9a9006842cb65d7ea8d32c4d20405368b231bd7
Paolo Chiodi <chiodi84@gmail.com>
2020-10-15 11:28:33 +0200

    add linting

--- test/build-plugin.test.js
+++ test/build-plugin.test.js
@@ -8,14 +8,14 @@ const buildPlugin = proxyquire('../lib/build-plugin', {
 })
 
 class Client {
-  get (key) {
+  get(key) {
     return `content for ${key}`
   }
 }
 
 beforeEach(async () => sinon.restore())
 
-test('builds a fastify plugin', t => {
+test('builds a fastify plugin', (t) => {
   buildPlugin(Client, {
     option: 'option1'
   })
@@ -30,7 +30,7 @@ test('builds a fastify plugin', t => {
   t.end()
 })
 
-test('plugin', t => {
+test('plugin', (t) => {
   t.plan(7)
 
   buildPlugin(Client, {
@@ -38,94 +38,112 @@ test('plugin', t => {
   })
   const plugin = fp.firstCall.args[0]
 
-  t.test('no namespace', async t => {
+  t.test('no namespace', async (t) => {
     t.plan(2)
 
     const decorate = sinon.spy()
 
-    await plugin({ decorate }, {
-      secrets: {
-        secret1: 'secret1-name',
-        secret2: 'secret2-name'
+    await plugin(
+      { decorate },
+      {
+        secrets: {
+          secret1: 'secret1-name',
+          secret2: 'secret2-name'
+        }
       }
-    })
+    )
 
     t.ok(decorate.called, 'decorates fastify')
-    t.ok(decorate.calledWith('secrets', {
-      secret1: 'content for secret1-name',
-      secret2: 'content for secret2-name'
-    }), 'decorates with secrets content')
+    t.ok(
+      decorate.calledWith('secrets', {
+        secret1: 'content for secret1-name',
+        secret2: 'content for secret2-name'
+      }),
+      'decorates with secrets content'
+    )
   })
 
-  t.test('no namespace - secrets exists', async t => {
+  t.test('no namespace - secrets exists', async (t) => {
     t.plan(2)
 
     const decorate = sinon.spy()
 
-    const promise = plugin({ decorate, secrets: {} }, {
-      secrets: {
-        secret1: 'secret1-name',
-        secret2: 'secret2-name'
+    const promise = plugin(
+      { decorate, secrets: {} },
+      {
+        secrets: {
+          secret1: 'secret1-name',
+          secret2: 'secret2-name'
+        }
       }
-    })
+    )
 
-    await t.rejects(
-      promise,
-      new Error('fastify-secrets has already been registered'),
-      'registration fails'
-     )
+    await t.rejects(promise, new Error('fastify-secrets has already been registered'), 'registration fails')
     t.notOk(decorate.called, 'does not decorate fastify')
   })
 
-  t.test('namespace', async t => {
+  t.test('namespace', async (t) => {
     t.plan(2)
 
     // emulate decorate behaviour
-    const decorate = sinon.stub().callsFake(function decorate (key, obj) {
+    const decorate = sinon.stub().callsFake(function decorate(key, obj) {
       this[key] = obj
     })
 
-    await plugin({ decorate }, {
-      namespace: 'namespace1',
-      secrets: {
-        secret1: 'secret1-name',
-        secret2: 'secret2-name'
+    await plugin(
+      { decorate },
+      {
+        namespace: 'namespace1',
+        secrets: {
+          secret1: 'secret1-name',
+          secret2: 'secret2-name'
+        }
       }
-    })
+    )
 
     t.ok(decorate.called, 'decorates fastify')
-    t.ok(decorate.calledWith('secrets', {
-      namespace1: {
-        secret1: 'content for secret1-name',
-        secret2: 'content for secret2-name'
-      }
-    }), 'decorates with secrets content')
+    t.ok(
+      decorate.calledWith('secrets', {
+        namespace1: {
+          secret1: 'content for secret1-name',
+          secret2: 'content for secret2-name'
+        }
+      }),
+      'decorates with secrets content'
+    )
   })
 
-  t.test('namespace - secrets exists', async t => {
+  t.test('namespace - secrets exists', async (t) => {
     t.plan(2)
 
     const decorate = sinon.spy()
     const secrets = {}
 
-    await plugin({ decorate, secrets }, {
-      namespace: 'namespace1',
-      secrets: {
-        secret1: 'secret1-name',
-        secret2: 'secret2-name'
+    await plugin(
+      { decorate, secrets },
+      {
+        namespace: 'namespace1',
+        secrets: {
+          secret1: 'secret1-name',
+          secret2: 'secret2-name'
+        }
       }
-    })
+    )
 
     t.notOk(decorate.called, 'does not decorate fastify')
-    t.same(secrets, {
-      namespace1: {
-        secret1: 'content for secret1-name',
-        secret2: 'content for secret2-name'
-      }
-    }, 'adds secrets content')
+    t.same(
+      secrets,
+      {
+        namespace1: {
+          secret1: 'content for secret1-name',
+          secret2: 'content for secret2-name'
+        }
+      },
+      'adds secrets content'
+    )
   })
 
-  t.test('namespace - namespace exists', async t => {
+  t.test('namespace - namespace exists', async (t) => {
     t.plan(3)
 
     const decorate = sinon.spy()
@@ -133,13 +151,16 @@ test('plugin', t => {
       namespace1: {}
     }
 
-    const promise = plugin({ decorate, secrets }, {
-      namespace: 'namespace1',
-      secrets: {
-        secret1: 'secret1-name',
-        secret2: 'secret2-name'
+    const promise = plugin(
+      { decorate, secrets },
+      {
+        namespace: 'namespace1',
+        secrets: {
+          secret1: 'secret1-name',
+          secret2: 'secret2-name'
+        }
       }
-    })
+    )
 
     await t.rejects(
       promise,
@@ -147,40 +168,36 @@ test('plugin', t => {
       'registration fails'
     )
     t.notOk(decorate.called, 'does not decorate fastify')
-    t.notSame(secrets, {
-      namespace1: {
-        secret1: 'content for secret1-name',
-        secret2: 'content for secret2-name'
-      }
-    }, 'does not add secrets content')
+    t.notSame(
+      secrets,
+      {
+        namespace1: {
+          secret1: 'content for secret1-name',
+          secret2: 'content for secret2-name'
+        }
+      },
+      'does not add secrets content'
+    )
   })
 
-  t.test('no options', async t => {
+  t.test('no options', async (t) => {
     t.plan(2)
 
     const decorate = sinon.spy()
     const promise = plugin({ decorate })
 
-    await t.rejects(
-      promise,
-      new Error(`fastify-secrets: no secrets requested`),
-      'registration fails'
-    )
+    await t.rejects(promise, new Error(`fastify-secrets: no secrets requested`), 'registration fails')
     t.notOk(decorate.called, 'does not decorate fastify')
   })
 
-  t.test('no secrets', async t => {
+  t.test('no secrets', async (t) => {
     t.plan(2)
 
     const decorate = sinon.spy()
     const emptyOpts = {}
     const promise = plugin({ decorate }, emptyOpts)
 
-    await t.rejects(
-      promise,
-      new Error(`fastify-secrets: no secrets requested`),
-      'registration fails'
-    )
+    await t.rejects(promise, new Error(`fastify-secrets: no secrets requested`), 'registration fails')
     t.notOk(decorate.called, 'does not decorate fastify')
   })
 })
