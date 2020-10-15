b270b5a058ce50153a386bf311290a12bfbac1d0
Paolo Chiodi <chiodi84@gmail.com>
2020-10-15 17:11:23 +0200

    feat: add base functionality (#1)

--- README.md
+++ README.md
@@ -2,6 +2,95 @@
 
 Simplify development of fastify-secrets plugins
 
+> This module is intended for developers implemeting fastify-secrets plugins, not for developers using fastify-plugin in their fastify projects
+
+## Installation
+
+```
+npm install --save fastify-secrets-core
+```
+
+## Usage
+
+`fastify-secrets-core` simplifies development of `fastify-secrets` plugins and ensures that all plugins implements the same api.
+
+Each plugin must only provide a simple client that interacts with the secrets storage and `fastify-secrets-core` will glue it to fastify.
+
+### buildPlugin
+
+Create a new fastify plugin by calling `buildPlugin` and providing a client class or constructor and options that will be forwarded to `fastify-plugin`.
+
+`buildPlugin` will return a fastify plugin that can be exported.
+
+#### Example
+
+```js
+const { buildPlugin } = require('fastify-secrets-core')
+const Client = require('./client')
+
+const plugin = buildPlugin(Client, {
+  name: 'fastify-secrets-example'
+})
+```
+
+### Client
+
+The client that needs to be provided to `fastify-secrets-core` should implement this methods:
+
+- `constructor`
+- `async get(ref)` (required) This will be called to fetch a single secret identified by reference. It's up to the implementation what ref is.
+- `async close()` (optional) This method will be called, if present, when the plugin is done fetching the secrets to give a chance to implementations to close any connection or run any teardown code. No further calls to the `get` method will be run after close.
+
+### Example
+
+This is a basic client implementation that reads secrets from `process.env`
+
+```js
+class Client {
+  constructor() {
+    console.log('client contructor')
+  }
+
+  async get(ref) {
+    return process.env[ref]
+  }
+
+  async close() {
+    console.log('client close')
+  }
+}
+
+```
+
+### Resulting plugin
+
+The resulting plugin can be registered directly in fastify.
+These are the available options:
+
+- `secrets` (required). A non-empty object representing a map of secret keys and references. The plugin will decorate fastify with a `secrets` object with the same keys as this option but will replace the references with the actual values for the secret as fetched with `client.get(reference)`
+- `namespace` (optional). If present, the plugin will add the secret values to `fastify.secrets.namespace` instead of `fastify.secrets`.
+- `concurrency` (optional, defaults to 5). How  many concurrent call will be made to `client.get`. This is handled by `fastify-secrets-core` and it's transparent to the implementation.
+
+#### Example
+
+Assuming a plugin is built as per the previous examples, it can be used as
+
+```js
+fastify.register(plugin, {
+  namespace: 'db',
+  concurrency: 5,
+  secrets: {
+    user: 'PG_USER',
+    pass: 'PG_PASS'
+  }
+})
+
+await fastify.ready()
+
+console.log(fastify.secrets.db.pass)
+
+```
+
 ## Contributing
 
 See [CONTRIBUTING.md](./CONTRIBUTING.md)
---
+++ test/build-plugin.test.js
@@ -0,0 +1,276 @@
+'use strict'
+
+const { test, beforeEach } = require('tap')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
+
+const fp = sinon.spy()
+const buildPlugin = proxyquire('../lib/build-plugin', {
+  'fastify-plugin': fp
+})
+
+class Client {
+  async get(key) {
+    return `content for ${key}`
+  }
+
+  async close() {}
+}
+
+beforeEach(async () => {
+  fp.resetHistory()
+  sinon.resetBehavior()
+  sinon.resetHistory()
+  sinon.reset()
+  sinon.restore()
+})
+
+test('builds a fastify plugin', (t) => {
+  buildPlugin(Client, {
+    option: 'option1'
+  })
+
+  t.ok(fp.called, 'calls fastify-plugin')
+
+  const opts = fp.firstCall.args[1]
+
+  t.equal(opts.fastify, '3.x', 'adds option for fastify support')
+  t.equal(opts.option, 'option1', 'forward provided options')
+
+  t.end()
+})
+
+test('plugin', (t) => {
+  t.plan(7)
+
+  buildPlugin(Client, {
+    option: 'option1'
+  })
+  const plugin = fp.firstCall.args[0]
+
+  t.test('no namespace', async (t) => {
+    t.plan(2)
+
+    const decorate = sinon.spy()
+
+    await plugin(
+      { decorate },
+      {
+        secrets: {
+          secret1: 'secret1-name',
+          secret2: 'secret2-name'
+        }
+      }
+    )
+
+    t.ok(decorate.called, 'decorates fastify')
+    t.ok(
+      decorate.calledWith('secrets', {
+        secret1: 'content for secret1-name',
+        secret2: 'content for secret2-name'
+      }),
+      'decorates with secrets content'
+    )
+  })
+
+  t.test('no namespace - secrets exists', async (t) => {
+    t.plan(2)
+
+    const decorate = sinon.spy()
+
+    const promise = plugin(
+      { decorate, secrets: {} },
+      {
+        secrets: {
+          secret1: 'secret1-name',
+          secret2: 'secret2-name'
+        }
+      }
+    )
+
+    await t.rejects(promise, new Error('fastify-secrets has already been registered'), 'registration fails')
+    t.notOk(decorate.called, 'does not decorate fastify')
+  })
+
+  t.test('namespace', async (t) => {
+    t.plan(2)
+
+    // emulate decorate behaviour
+    const decorate = sinon.stub().callsFake(function decorate(key, obj) {
+      this[key] = obj
+    })
+
+    await plugin(
+      { decorate },
+      {
+        namespace: 'namespace1',
+        secrets: {
+          secret1: 'secret1-name',
+          secret2: 'secret2-name'
+        }
+      }
+    )
+
+    t.ok(decorate.called, 'decorates fastify')
+    t.ok(
+      decorate.calledWith('secrets', {
+        namespace1: {
+          secret1: 'content for secret1-name',
+          secret2: 'content for secret2-name'
+        }
+      }),
+      'decorates with secrets content'
+    )
+  })
+
+  t.test('namespace - secrets exists', async (t) => {
+    t.plan(2)
+
+    const decorate = sinon.spy()
+    const secrets = {}
+
+    await plugin(
+      { decorate, secrets },
+      {
+        namespace: 'namespace1',
+        secrets: {
+          secret1: 'secret1-name',
+          secret2: 'secret2-name'
+        }
+      }
+    )
+
+    t.notOk(decorate.called, 'does not decorate fastify')
+    t.same(
+      secrets,
+      {
+        namespace1: {
+          secret1: 'content for secret1-name',
+          secret2: 'content for secret2-name'
+        }
+      },
+      'adds secrets content'
+    )
+  })
+
+  t.test('namespace - namespace exists', async (t) => {
+    t.plan(3)
+
+    const decorate = sinon.spy()
+    const secrets = {
+      namespace1: {}
+    }
+
+    const promise = plugin(
+      { decorate, secrets },
+      {
+        namespace: 'namespace1',
+        secrets: {
+          secret1: 'secret1-name',
+          secret2: 'secret2-name'
+        }
+      }
+    )
+
+    await t.rejects(
+      promise,
+      new Error(`fastify-secrets 'namespace1' instance name has already been registered`),
+      'registration fails'
+    )
+    t.notOk(decorate.called, 'does not decorate fastify')
+    t.notSame(
+      secrets,
+      {
+        namespace1: {
+          secret1: 'content for secret1-name',
+          secret2: 'content for secret2-name'
+        }
+      },
+      'does not add secrets content'
+    )
+  })
+
+  t.test('no options', async (t) => {
+    t.plan(2)
+
+    const decorate = sinon.spy()
+    const promise = plugin({ decorate })
+
+    await t.rejects(promise, new Error(`fastify-secrets: no secrets requested`), 'registration fails')
+    t.notOk(decorate.called, 'does not decorate fastify')
+  })
+
+  t.test('no secrets', async (t) => {
+    t.plan(2)
+
+    const decorate = sinon.spy()
+    const emptyOpts = {}
+    const promise = plugin({ decorate }, emptyOpts)
+
+    await t.rejects(promise, new Error(`fastify-secrets: no secrets requested`), 'registration fails')
+    t.notOk(decorate.called, 'does not decorate fastify')
+  })
+})
+
+test('client integration', (t) => {
+  t.plan(2)
+
+  t.test('client with close method', async (t) => {
+    t.plan(1)
+
+    let closeCalled = false
+
+    class Client {
+      async get(key) {
+        return key
+      }
+
+      async close() {
+        closeCalled = true
+      }
+    }
+
+    buildPlugin(Client)
+    const plugin = fp.firstCall.args[0]
+
+    const decorate = sinon.spy()
+
+    await plugin(
+      { decorate },
+      {
+        secrets: {
+          secret1: 'secret1-name',
+          secret2: 'secret2-name'
+        }
+      }
+    )
+
+    t.ok(closeCalled, 'calls client.close if present')
+  })
+
+  t.test('client without close method', async (t) => {
+    t.plan(1)
+
+    class Client {
+      async get(key) {
+        return key
+      }
+    }
+
+    buildPlugin(Client)
+    const plugin = fp.firstCall.args[0]
+    const decorate = sinon.spy()
+
+    const promise = plugin(
+      { decorate },
+      {
+        secrets: {
+          secret1: 'secret1-name',
+          secret2: 'secret2-name'
+        }
+      }
+    )
+
+    await t.resolves(promise, 'does not fail')
+  })
+})
